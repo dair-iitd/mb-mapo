6061c5912ddae288397d14fc364ac59a4a6940d6
NikhilGupta1997 <nikhilgupta1997@gmail.com>
2019-06-11 20:30:59 +0530

    Removed cell state in decoder

--- src/memn2n/attention_wrapper.py
+++ src/memn2n/attention_wrapper.py
@@ -897,10 +897,15 @@ class AttentionWrapper(rnn_cell_impl.RNNCell):
     # Step 1: Calculate the true inputs to the cell based on the
     # previous attention value.
     inputs, position_emb = inputs
-    cell_inputs = self._cell_input_fn(inputs, state.attention)
-    cell_inputs = self._cell_input_fn(cell_inputs, position_emb)
+    # cell_inputs = self._cell_input_fn(inputs, state.attention)
+    # cell_inputs = self._cell_input_fn(cell_inputs, position_emb)
+    # cell_inputs = self._cell_input_fn(position_emb, state.attention)
+
+    cell_inputs = position_emb
     cell_state = state.cell_state
     cell_output, next_cell_state = self._cell(cell_inputs, cell_state)
+    
+    next_cell_state = cell_state
 
     cell_batch_size = (
         cell_output.shape[0].value or array_ops.shape(cell_output)[0])
